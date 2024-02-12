02b443acee337e47f8c19b79df882c606176899c
hir0theboy <lipealves2345meia78@gmail.com>
2024-02-11 23:24:18 -0300

    primeiro commit

---
+++ src/containers/Produtos.tsx
@@ -0,0 +1,40 @@
+import { useSelector } from 'react-redux'
+import { Produto as ProdutoType } from '../App'
+import Produto from '../components/Produto'
+
+import * as S from './styles'
+import { RootReducer } from '../store'
+import { useGetProdutosQuery } from '../services/api'
+
+const ProdutosComponent = () => {
+  const { data: produtos, isLoading } = useGetProdutosQuery()
+
+  const itensFavoritos = useSelector(
+    (state: RootReducer) => state.favoritos.itens
+  )
+
+  const produtoEstaNosFavoritos = (produto: ProdutoType) => {
+    const produtoId = produto.id
+    const IdsDosFavoritos = itensFavoritos.map((f) => f.id)
+
+    return IdsDosFavoritos.includes(produtoId)
+  }
+
+  if (isLoading) return <h2>Carregando...</h2>
+
+  return (
+    <>
+      <S.Produtos>
+        {produtos?.map((produto) => (
+          <Produto
+            estaNosFavoritos={produtoEstaNosFavoritos(produto)}
+            key={produto.id}
+            produto={produto}
+          />
+        ))}
+      </S.Produtos>
+    </>
+  )
+}
+
+export default ProdutosComponent
